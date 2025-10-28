c78e1cac19cb113b1ba7e3573a8e02a63a1810ff
r_podd <rpoddar05@gmail.com>
2025-10-27 22:29:20 -0400

    test: integrate Testcontainers PostgreSQL setup and enable CaseApiIT end-to-end test

--- src/test/java/com/phep/casesvc/CasesvcApplicationTests.java
+++ src/test/java/com/phep/casesvc/CasesvcApplicationTests.java
@@ -1,10 +1,11 @@
 package com.phep.casesvc;
 
+import com.phep.casesvc.testsupport.DbIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CasesvcApplicationTests {
+class CasesvcApplicationTests extends DbIntegrationTest {
 
     @Test
     void contextLoads() {
